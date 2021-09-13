caa2571e4bc1e5abe56567cd5eaa9a066db1b4ea
Luca Casonato <hello@lcas.dev>
2021-09-13 16:01:16 +0200

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+addEventListener(
+  "fetch",
+  (event) => event.respondWith(handleRequest(event.request)),
+);
+
+/**
+ * @param {Request} request
+ * @returns {Response}
+ */
+function handleRequest(request) {
+  return new Response("Hello World");
+}
