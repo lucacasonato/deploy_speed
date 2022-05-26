fa3f86d028cfe028262f535794d9bfe916303c0b
Luca Casonato <hello@lcas.dev>
2022-05-26 14:21:27 +0200

    fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-  
-function handler(req: Request): Response {
+
+function handler(req) {
   return new Response("Hello world");
 }
 
