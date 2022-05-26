e1a48cd3ad3066cac3f5671c0d54610a3b01e3d1
Luca Casonato <hello@lcas.dev>
2022-05-26 14:26:59 +0200

    test dabb4623-ef00-4945-802c-666fa0c280af

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 function handler(req) {
-  return new Response("Hello world");
+  return new Response("dabb4623-ef00-4945-802c-666fa0c280af");
 }
 
 serve(handler);
