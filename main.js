e3f33cc1528a4ac43bb7de662fd15d62c39475b9
Luca Casonato <hello@lcas.dev>
2022-05-27 13:46:00 +0200

    test 33ea056e-be40-4bbc-b3ee-94a35f264fb7

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 function handler(req) {
-  return new Response("f0e38825-2062-4493-9700-55191e451a9f");
+  return new Response("33ea056e-be40-4bbc-b3ee-94a35f264fb7");
 }
 
 serve(handler);
